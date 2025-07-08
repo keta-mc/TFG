d43a12c07473c3442744268ca94301cd2ab7e03a
Catalina Mor'an <ketamoran@gmail.com>
2025-07-08 16:47:33 +0200

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tfg# TFG
+Diseño e Implementación de un sistema de transcripción de partituras en imágenes a sonido mediante reconocimiento de notas
 # TFG
--- abrirImg.py
+++
@@ -1,6 +0,0 @@
-import os
-import cv2
-from preprocessing import cleanAndBinarize, staffLineDetectionAndRemoval
-
-cleanAndBinarize("data/images/000100134-4_1_1.png")
-staffLineDetectionAndRemoval()
